3a40362cc543a9763bbd7175f8b01243a2a060da
wpangestu <wa.pangestu16@gmail.com>
2023-09-02 06:42:19 +0700

    unit test register n login

---
+++ test/test-util.js
@@ -0,0 +1,23 @@
+import { prismaClient } from "../src/application/database";
+import bcrypt from "bcryptjs";
+
+const removeTestUser = async () => {
+  await prismaClient.user.deleteMany({
+    where: {
+      username: "wpangestu",
+    },
+  });
+};
+
+const createTestUser = async () => {
+  await prismaClient.user.create({
+    data: {
+      username: "wpangestu",
+      password: await bcrypt.hash("rahasia", 10),
+      name: "Wahyu Pangestu",
+      token: "test",
+    },
+  });
+};
+
+export { removeTestUser, createTestUser };
--- test/user.test.js
+++ test/user.test.js
@@ -1,15 +1,11 @@
 import supertest from "supertest";
 import { web } from "../src/application/web";
-import { prismaClient } from "../src/application/database";
 import { logger } from "../src/application/logging";
+import { createTestUser, removeTestUser } from "./test-util";
 
 describe("POST /api/users", function () {
   afterEach(async () => {
-    await prismaClient.user.deleteMany({
-      where: {
-        username: "wpangestu",
-      },
-    });
+    await removeTestUser();
   });
 
   it("should can register user", async () => {
@@ -59,5 +55,72 @@ describe("POST /api/users", function () {
     logger.info(result.body);
     expect(result.status).toBe(400);
     expect(result.body.errors).toBeDefined();
+  });
+});
+
+describe("POST /api/users/login", function () {
+  beforeEach(async () => {
+    await createTestUser();
+  });
+
+  afterEach(async () => {
+    await removeTestUser();
+  });
+
+  it("it should can login", async () => {
+    const result = await supertest(web).post("/api/users/login").send({
+      username: "wpangestu",
+      password: "rahasia",
+    });
+
+    expect(result.status).toBe(200);
+    expect(result.body.data.token).toBeDefined();
+    expect(result.body.data.token).not.toBe("test");
+  });
+
+  it("it should rejected empty username n password", async () => {
+    const result = await supertest(web).post("/api/users/login").send({
+      username: "",
+      password: "",
+    });
+
+    expect(result.status).toBe(400);
+    expect(result.body.errors).toBeDefined();
+  });
+
+  it("it should rejected wrong username n password", async () => {
+    const result = await supertest(web).post("/api/users/login").send({
+      username: "salah",
+      password: "salah",
+    });
+
+    logger.info(result);
+    expect(result.status).toBe(401);
+    expect(result.body.errors).toBeDefined();
+    expect(result.body.errors).toBe("Username or password wrong");
+  });
+
+  it("it should rejected wrong password", async () => {
+    const result = await supertest(web).post("/api/users/login").send({
+      username: "wpangestu",
+      password: "salah",
+    });
+
+    logger.info(result);
+    expect(result.status).toBe(401);
+    expect(result.body.errors).toBeDefined();
+    expect(result.body.errors).toBe("Username or password wrong");
+  });
+
+  it("it should rejected wrong username", async () => {
+    const result = await supertest(web).post("/api/users/login").send({
+      username: "salah",
+      password: "rahasia",
+    });
+
+    logger.info(result);
+    expect(result.status).toBe(401);
+    expect(result.body.errors).toBeDefined();
+    expect(result.body.errors).toBe("Username or password wrong");
   });
 });
